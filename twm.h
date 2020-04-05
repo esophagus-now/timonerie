9b2c6963177108fc93a5971fdd1532a798c8d2ac
Marco Merlini <mahkoe@gmail.com>
2020-04-05 12:52:37 -0400

    Still adding things

--- twm.h
+++ twm.h
@@ -6,11 +6,17 @@
 //////////////////////////////////////////////////
 
 extern char const *const TWM_SUCC; // = "success";
-extern char const *const TWM_LEAF_SZ_ERR; // = "leaf node size check failed";
-extern char const *const TWM_LEAF_DRAW_ERR; // = "leaf node draw failed";
+extern char const *const TWM_TWM_LEAF_SZ_ERR; // = "TWM_LEAF node size check failed";
+extern char const *const TWM_TWM_LEAF_DRAW_ERR; // = "TWM_LEAF node draw failed";
 extern char const *const TWM_BAD_NODE_TYPE; // = "invalid node type (uninitialized memory?)";
 extern char const *const TWM_BAD_SZ; // = "bad node size";
 extern char const *const TWM_BAD_POS; // = "bad node position";
+extern char const *const TWM_FULL_PARENT; // = "too many children";
+extern char const *const TWM_NULL_DRAW_FN; // = "window has no draw function";
+extern char const *const TWM_NULL_DRAW_SZ; // = "window has no size-finding function";
+extern char const *const TWM_NULL_TRIG_REDRAW; // = "window has no redraw trigger function";
+extern char const *const TWM_NO_WINDOWS; // = "no windows in tree";
+extern char const *const TWM_INVALID_TREE; // = "window tree is in a broken state!";
 
 //Draws item. Returns number of bytes added into buf, or -1 on error.
 typedef int draw_fn_t(void *item, int x, int y, int w, int h, char *buf);
@@ -19,7 +25,7 @@ typedef int draw_fn_t(void *item, int x, int y, int w, int h, char *buf);
 //given the size
 typedef int draw_sz_t(void *item, int w, int h);
 
-//Tells item that it shoudl redraw, probably because it moved to another
+//Tells item that it should redraw, probably because it moved to another
 //area of the screen
 typedef void trigger_redraw_t(void *item);
 
@@ -36,28 +42,34 @@ typedef struct _draw_operations {
 } draw_operations;
 
 typedef enum _twm_node_type {
-    LEAF,
-    HORZ,
-    VERT
+    EMPTY = 0,
+    TWM_LEAF,
+    TWM_HORZ,
+    TWM_VERT
 } twm_node_type;
 
+//These should be opaque structs
 #define MAX_CHILDREN 8
 typedef struct _twm_node {
-    //If this is a leaf node, then draw the item pointed to by item_to_draw
+    //If this is a TWM_LEAF node, then draw the item pointed to by item_to_draw
     //using the function pointed at by how_to_draw_it. Otherwise, we need 
     //to recurse and draw each of the children
     twm_node_type type;
+    struct _twm_node *parent;
     
     //This means that this node's borders will be redrawn. Has no bearing 
-    //on children, and is unused when type == LEAF
+    //on children, and is unused when type == TWM_LEAF
     int need_redraw;
     
-    //If this is a leaf, simply draw the item. The pointer and function
+    //This node and all its children will be highlighted
+    int has_focus;
+    
+    //If this is a TWM_LEAF, simply draw the item. The pointer and function
     //pointers contain all the necessary instructions to do it.
     void *item;
     draw_operations draw_ops;
     
-    //Otherwise, this node either contains vertically or horizontally 
+    //Otherwise, this node either contains TWM_VERTically or horizontally 
     //stacked "windows"
     struct _twm_node *children[MAX_CHILDREN];
     int num_children;
@@ -65,6 +77,17 @@ typedef struct _twm_node {
     //Error information
     char const *error_str;
 } twm_node;
+
+typedef struct _twm_tree {
+    twm_node *head;
+    twm_node *focus; //Points to the window that the user is "focused" on
+                     //This is used for positioning new windows, and if I
+                     //later decide to draw borders differently around the
+                     //focused window
+    
+    //Error informaiton
+    char const *error_str;
+} twm_tree;
 
 //Here's the key idea: twm nodes also know how to draw themselves
 int draw_fn_twm_node(void *item, int x, int y, int w, int h, char *buf) {
@@ -86,17 +109,28 @@ int draw_fn_twm_node(void *item, int x, int y, int w, int h, char *buf) {
     }
     
     //Draw this node
-    if (t->type == LEAF) {
-        //Return bytes consumed by the leaf node
+    char *buf_saved = buf; //Keep track of original position so we can calculate size
+    if (t->has_focus) {
+        *buf++ = '\e'; *buf++ = '['; *buf++ = '1'; *buf++ = 'm'; //Turn on highlight mode
+    }
+    if (t->type == TWM_LEAF) {
+        //Check if a proper draw fn is given
+        if (t->draw_ops.draw_fn == NULL) {
+            t->error_str = TWM_NULL_DRAW_FN;
+            return -1;
+        }
+        //Return bytes consumed by the TWM_LEAF node
         int child_sz = t->draw_ops.draw_fn(t->item, x, y, w, h, buf);
         if (child_sz < 0) {
-            t->error_str = TWM_LEAF_DRAW_ERR;
+            t->error_str = TWM_TWM_LEAF_DRAW_ERR;
         }
-        return child_sz;
-    } else if (t->type == HORZ) {
-        //Save current buf pos, so we can calculate number of bytes we added
-        char *buf_saved = buf;
+        buf += child_sz;
         
+        if (t->has_focus) {
+            *buf++ = '\e'; *buf++ = '['; *buf++ = 'm'; //Restore original mode
+        }
+        return buf_saved - buf;
+    } else if (t->type == TWM_HORZ) {        
         //In a perfect world, the width (minus the columns needed for drawing
         //separating borders) is an exact multiple of the number of children.
         //However, here we're forced to keep track of how much error we have
@@ -146,14 +180,15 @@ int draw_fn_twm_node(void *item, int x, int y, int w, int h, char *buf) {
             child_x++; //Advance past border line
         }
         
+        if (t->has_focus) {
+            *buf++ = '\e'; *buf++ = '['; *buf++ = 'm'; //Restore original mode
+        }
+        
         t->need_redraw = 0;
         
         t->error_str = TWM_SUCC;
         return buf - buf_saved;
-    } else if (t->type == VERT) {
-        //Save current buf pos, so we can calculate number of bytes we added
-        char *buf_saved = buf;
-        
+    } else if (t->type == TWM_VERT) {        
         //In a perfect world, the width (minus the columns needed for drawing
         //separating borders) is an exact multiple of the number of children.
         //However, here we're forced to keep track of how much error we have
@@ -202,6 +237,10 @@ int draw_fn_twm_node(void *item, int x, int y, int w, int h, char *buf) {
             child_y++; //Advance past border line
         }
         
+        if (t->has_focus) {
+            *buf++ = '\e'; *buf++ = '['; *buf++ = 'm'; //Restore original mode
+        }
+        
         t->need_redraw = 0;
         
         t->error_str = TWM_SUCC;
@@ -222,16 +261,26 @@ int draw_sz_twm_node(void *item, int w, int h) {
         return -1;
     }
     
+    int total_sz = 0;
+    if (t->has_focus) {
+        total_sz += 7; //Extra characters needed to turn on highlight and
+                       //ten turn it off afterwards
+    }
+        
     //Calculate size
-    if (t->type == LEAF) {
+    if (t->type == TWM_LEAF) {
+        //Check if a valid size function was given
+        if (t->draw_ops.draw_sz == NULL) {
+            t->error_str = TWM_NULL_DRAW_SZ;
+            return -1;
+        }
         //Simply return the amount of space needed by the contained drawable
         int child_sz = t->draw_ops.draw_sz(t->item, w, h);
         if (child_sz < 0) {
-            t->error_str = TWM_LEAF_SZ_ERR;
+            t->error_str = TWM_TWM_LEAF_SZ_ERR;
         }
         return child_sz;
-    } else if (t->type == HORZ) {
-        int total_sz = 0;
+    } else if (t->type == TWM_HORZ) {
         
         //In a perfect world, the width (minus the columns needed for drawing
         //separating borders) is an exact multiple of the number of children.
@@ -274,9 +323,7 @@ int draw_sz_twm_node(void *item, int w, int h) {
         
         t->error_str = TWM_SUCC;
         return total_sz;
-    } else if (t->type == VERT) {
-        int total_sz = 0;
-        
+    } else if (t->type == TWM_VERT) {        
         //In a perfect world, the height (minus the rows needed for drawing
         //separating borders) is an exact multiple of the number of children.
         //However, here we're forced to keep track of how much error we have
@@ -330,5 +377,147 @@ void trigger_redraw_twm_node(void *item) {
     if (t != NULL) t->need_redraw = 1;
 }
 
+//Special functions for empty nodes
+int draw_fn_empty(void *item, int x, int y, int w, int h, char *buf) {return 0;}
+int draw_sz_empty(void *item, int w, int h) {return 0;}
+void trigger_redraw_empty(void *item) {return;}
+
+draw_operations const empty_ops = {
+    .draw_fn = draw_fn_empty,
+    .draw_sz = draw_sz_empty,
+    .trigger_redraw = trigger_redraw_empty
+};
+
+//Returns a newly allocated twm_node with given drawable, or NULL on error
+static twm_node* construct_TWM_LEAF_twm_node(void *item, draw_operations draw_ops) {
+    twm_node *ret = calloc(1, sizeof(twm_node));
+    if (!ret) return NULL;
+    
+    ret->type = TWM_LEAF;
+    ret->item = item;
+    ret->draw_ops = draw_ops;
+    
+    return ret;
+}
+
+//Free twm_node. Gracefully ignores NULL
+static void destroy_twm_node(twm_node *t) {
+    if (t == NULL) return;
+    
+    free(t);
+}
+
+//Free twm_node tree rooted at this node. Gracefully ignores NULL input
+static void free_twm_node_tree(twm_node *head) {
+    if (head == NULL) return;
+    
+    if (head->type == TWM_LEAF) {
+        free(head);
+        return;
+    } else {
+        int i;
+        for (i = 0; i < head->num_children; i++) {
+            free_twm_node_tree(head->children[i]);
+        }
+        free(head);
+        return;
+    }
+}
+
+//Mark all nodes in the subtree for redraw. Returns -1 on error (and sets
+//t->error_str), or -2 if t was NULL
+static int redraw_twm_node_tree(twm_node *t) {
+    if (t == NULL) {
+        return -2; //This is all we can do
+    }
+    
+    if (t->type == TWM_LEAF) {
+        
+    }
+}
+
+//Returns a new twm_tree on success, NULL on error
+twm_tree* new_twm_tree() {
+    twm_tree *ret = calloc(1, sizeof(twm_tree));
+    return ret;
+}
+
+//Frees twm_tree and all associated resources. Gracefully ignores NULL input
+void del_twm_tree(twm_tree *t) {
+    if (t == NULL) return;
+    
+    free_twm_node_tree(t->head);
+    
+    free(t);
+}
+
+//Inserts a new drawable, based on t's currently focused window
+int twm_tree_add_window(twm_tree *t, void *item, draw_operations draw_ops) {
+    
+}
+
+//Directions for moving focus/windows
+typedef enum _twm_dir {
+    TWM_UP,
+    TWM_DOWN,
+    TWM_LEFT,
+    TWM_RIGHT,
+    TWM_PARENT,
+    TWM_CHILD
+} twm_dir;
+
+//Returns 0 on success, -1 on error (and sets t->error_str). Returns -2 if
+//t is NULL.
+int twm_tree_move_focus(twm_tree *t, twm_dir dir) {
+    if (t == NULL) {
+        return -2; //This is all we can do
+    }
+    
+    //Not currently focused on anything. Pick the "leftmost" child if one
+    //is available
+    if (t->focus == NULL) {
+        if (t->head == NULL) {
+            t->error_str = TWM_NO_WINDOWS;
+            return -1;
+        }
+        
+        twm_node *cur = t->head;
+        while (cur->type != TWM_LEAF) {
+            if (t->num_children == 0) {
+                t->error_str = TWM_INVALID_TREE;
+                return -1;
+            }
+            cur = t->children[0];
+            if (cur == NULL) {
+                t->error_str = TWM_INVALID_TREE;
+                return -1;
+            }
+        }
+        
+        t->focus = cur;
+        t->focus->has_focus = 1;
+        return 0;
+    }
+    
+    //If we are focused on the root of the tree, we can't move the focus
+    if (t->focus->parent == NULL) {
+        return 0;
+    }
+    
+    //The TWM_PARENT and TWM_CHILD directions are easy to implement
+    if (dir == TWM_PARENT) {
+        t->focus->has_focus = 0;
+        
+        t->focus = t->focus->parent;
+        t->focus->has_focus = 1;
+    }
+    
+    //Now the tricky business. Following "sane" rules, move focus to where
+    //the user intends
+    twm_node *cur = t->focus->parent;
+    while (1) {
+        
+    }
+}
 
 #endif
