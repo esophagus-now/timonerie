e71837d70cb2b0c21913e40e661d37317ecba9ce
Marco Merlini <mahkoe@gmail.com>
2020-04-07 14:38:03 -0400

    Started working on a fake software dbg guv to go between linux processes

--- Makefile
+++ Makefile
@@ -3,6 +3,9 @@
 main: main.c queue.h queue.c textio.h textio.c dbg_guv.h dbg_guv.c dbg_cmd.h dbg_cmd.c pollfd_array.h pollfd_array.c symtab.h symtab.c twm.h twm.c
 	gcc -g -o main -Wall -fno-diagnostics-show-caret main.c queue.c textio.c dbg_guv.c dbg_cmd.c pollfd_array.c symtab.c twm.c -lpthread -lreadline
 
+fake_dbg_guv: fake_dbg_guv.c
+	gcc -g -Wall -fno-diagnostics-show-caret fake_dbg_guv.c -lpthread
+
 clean:
 	rm -rf main
 	rm -rf *.o
---
+++ fake_dbg_guv.c
@@ -0,0 +1,244 @@
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>
+#include <poll.h>
+#include <pthread.h>
+#include <sched.h>
+#include <errno.h>
+
+//This is a quick and dirty dbg_guv simulator for use between Linux 
+//processes. I am forcing myself to not make it super good because I don't
+//want to spend too much time
+
+#define addr_drop_cnt      0 
+#define addr_log_cnt       1 
+#define addr_inj_TDATA     2 
+#define addr_inj_TVALID    3 
+#define addr_inj_TLAST     4 
+#define addr_inj_TKEEP     5 
+#define addr_inj_TDEST     6 
+#define addr_inj_TID       7 
+#define addr_keep_pausing  8 
+#define addr_keep_logging  9 
+#define addr_keep_dropping 10
+#define addr_dut_reset     11
+
+typedef struct _fake_dbg_guv {    
+    //S1 registers
+    unsigned keep_pausing;
+    unsigned keep_logging;
+    unsigned log_cnt;
+    unsigned keep_dropping;
+    unsigned drop_cnt;
+    unsigned inj_TDATA;
+    unsigned inj_TVALID;
+    unsigned inj_TKEEP;
+    unsigned inj_TLAST;
+    unsigned inj_TDEST;
+    unsigned inj_TID;
+    unsigned dut_reset;
+    
+    //S2 registers
+    unsigned keep_pausing_r;
+    unsigned keep_logging_r;
+    unsigned log_cnt_r;
+    unsigned keep_dropping_r;
+    unsigned drop_cnt_r;
+    unsigned inj_TDATA_r;
+    unsigned inj_TVALID_r;
+    unsigned inj_TKEEP_r;
+    unsigned inj_TLAST_r;
+    unsigned inj_TDEST_r;
+    unsigned inj_TID_r;
+    unsigned dut_reset_r;
+    
+    unsigned cmd_receipt_hdr;
+    unsigned cmd_receipt_hdr_sent;
+    unsigned cmd_receipt_tdata;
+    unsigned cmd_receipt_tvalid;
+    
+    unsigned log_hdr;
+    unsigned log_hdr_sent;
+    unsigned log_data;
+    unsigned log_data_sent;
+} fake_dbg_guv;
+
+//from https://stackoverflow.com/questions/12340695/how-to-check-if-a-given-file-descriptor-stored-in-a-variable-is-still-valid
+int fd_is_valid(int fd) {
+    return fcntl(fd, F_GETFD) != -1 || errno != EBADF;
+}
+
+volatile int stop = 0;
+void sigpipe_handler(int s) {
+    stop = 1;
+}
+
+char const *const SUCC = "succ";
+char const *const WRITE_ERROR = "write error";
+char const *const STOP_SIG = "stop was signaled";
+
+typedef struct {
+    int fd;
+    unsigned to_send;
+    unsigned vld;
+    unsigned val_sent;
+    unsigned stop;
+    char const *error_str;
+    pthread_mutex_t mutex;
+} egress_args;
+
+void *word_writer (void *arg) {
+    egress_args *args = (egress_args*)arg;
+    
+    while (1) {
+        unsigned local_copy;
+        int fd;
+        
+        pthread_mutex_lock(&arg->mutex);
+        //Save the value that the user is trying to send
+        local_copy = args->to_send;
+        fd = args->fd;
+        
+        //Check if we should stop
+        if (args->stop) {
+            args->error_str = STOP_SIG;
+            args->stop = 1;
+            pthread_mutex_unlock(&arg->mutex);
+            break;
+        }
+        
+        //Is there anything to send?
+        if (!args->vld) {
+            //Nothing to send
+            pthread_mutex_unlock(&arg->mutex);
+            sched_yield();
+            continue;
+        }
+        pthread_mutex_unlock(&arg->mutex);
+        
+        //At this point, there is something to send. Issue the write() call
+        int num_written = 0;
+        int rc;
+        //For extra safety, write this write in a loop. Probably unnecessary
+        //since we're no longer living in the 1960s
+        while (num_written < sizeof(local_copy)) {
+            rc = write(fd, ((char*)(&local_copy)) + num_written, sizeof(local_copy) - num_written);
+            if (rc <= 0) break;
+        }
+        
+        //Check for write error and set values
+        if (rc < 0) {
+            char *err = strerror(errno);
+            pthread_mutex_lock(&arg->mutex);
+            args->error_str = err;
+            args->stop = 1;
+            pthread_mutex_unlock(&arg->mutex);
+            break;
+        } else {
+            pthread_mutex_lock(&arg->mutex);
+            args->error_str = SUCC;
+            args->vld = 0;
+            args->val_sent = 1;
+            pthread_mutex_unlock(&arg->mutex);
+        }
+    }
+    
+    pthread_exit(NULL);
+}
+
+int main() {
+    //Try to make sure that the right file redirects were used
+    if (!fd_is_valid(3) || !fd_is_valid(4)) {
+        fprintf(stderr, "Please redirect pipe output to file 3 and file 4 to pipe input\n");
+        fflush(stderr);
+        return -1;
+    }
+    
+    fake_dbg_guv d;
+    memset(&d, 0, sizeof(d));
+    
+    //Hook up SIGPIPE handler to stop this program
+    sigaction sa = {
+        .sa_handler = sigpipe_handler,
+        .sa_flags = 0
+    };
+    sigemptyset(&sa.sa_mask);
+    sigaction(SIGPIPE, &sa, NULL);
+    
+    pthread_t out_tid, log_tid;
+    egress_args out_args = {
+        .fd = STDOUT_FILENO,
+        .vld = 0,
+        .stop = 0,
+        .error_str = SUCC,
+        .mutex = PTHREAD_MUTEX_INITIALIZER
+    };
+    egress_args log_args = {
+        .fd = 4,
+        .vld = 0,
+        .stop = 0,
+        .error_str = SUCC,
+        .mutex = PTHREAD_MUTEX_INITIALIZER
+    };
+    
+    pthread_create(&out_tid, NULL, word_writer, &out_args);
+    pthread_setname_np(out_tid, "dbg_guv out");
+    pthread_create(&log_tid, NULL, word_writer, &log_args);
+    pthread_setname_np(log_tid, "dbg_guv log");
+    
+    while (!stop) {
+        int output_ready = 0;
+        
+        //Check up on output thread
+        pthread_mutex_lock(&out_args.mutex);
+        if (out_args.stop) {
+            fprintf(stderr, "Output thread stopped early: %s\n", out_args.error_str);
+            pthread_mutex_unlock(&out_args.mutex);
+            break;
+        }
+        if (out_args.val_sent) {
+            //If an inject was sent, return inj_TVALID to 0
+            d.inj_TVALID = 0;
+        }
+        if (!out_args.vld) {
+            //See if there is anything available to write
+            if (d.inj_TVALID) {
+                out_args.to_send = d.inj_TDATA;
+                out_args.vld = 1;
+            } else {
+                //Later we'll check if anyone is trying to write to the 
+                //output, depending on pause/log/drop/inject flags
+                output_ready = 1;
+            }
+        }
+        
+    }
+    
+    //Try to properly close out the threads
+    int kill_out = 0, kill_log = 0;
+    pthread_mutex_lock(&out_args.mutex);
+    if (out_args.stop == 0) {
+        out_args.stop = 1;
+        kill_out = 1;
+    }
+    pthread_mutex_unlock(&out_args.mutex);
+    
+    pthread_mutex_lock(&log_args.mutex);
+    if (log_args.stop == 0) {
+        log_args.stop = 1;
+        kill_log = 1;
+    }
+    pthread_mutex_unlock(&log_args.mutex);
+    
+    //Interrupt any running system calls
+    if (kill_out) pthread_kill(out_tid, SIGCONT);
+    if (kill_log) pthread_kill(log_tid, SIGCONT);
+    
+    pthread_join(out_tid, NULL);
+    pthread_join(log_tid, NULL);
+    
+    return 0;
+}
