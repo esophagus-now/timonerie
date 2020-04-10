52702cc90bd2cb7d6aad1616e98c4cd992587fbc
Marco Merlini <mahkoe@gmail.com>
2020-04-10 01:38:10 -0400

    Oops, I always forget to commit new files

---
+++ timonier.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include "dbg_cmd.h"
+#include "twm.h"
+#include "dbg_guv.h"
+#include "timonier.h"
+#include "textio.h"
+
+//Default got_line function (interactive commands)
+static int got_line_inter(dbg_guv *owner, char const *str, dbg_cmd *dest) {
+    return parse_dbg_reg_cmd(dest, str);
+}
+
+static int lines_req_inter(dbg_guv *owner, int w, int h) {
+    //If this guv is tall enough, print a little message to remind the user
+    //that this guv under interactive control
+    if (h > 2) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+//Draws item. Returns number of bytes added into buf, or -1 on error.
+int draw_fn_inter(void *item, int x, int y, int w, int h, char *buf) {
+    //If draw_fn_dbg_guv gave us enough room, print a little message to
+    //remind the user that this guv under interactive control
+    if (h > 0) {
+        char *buf_saved = buf; //Keep track of original buf so we can count
+        //number of bytes written
+        
+        int incr = cursor_pos_cmd(buf, x, y); //Moving the cursor
+        buf += incr;
+        
+        sprintf(buf, UNDERLINE "%-*.*s" NO_UNDERLINE "%n", w, w, "Interactive mode", &incr);
+        buf += incr;
+        
+        return buf - buf_saved;
+    } else {
+        return 0;
+    }
+}
+
+//Returns how many bytes are needed (can be an upper bound) to draw item
+//given the size
+int draw_sz_inter(void *item, int w, int h) {
+    //If draw_fn_dbg_guv gave us enough room, print a little message to
+    //remind the user that this guv under interactive control
+    if (h > 0) {
+        int const cursor_mvmt = 10; //Moving the cursor
+        int const set_underline = 4 + 5; //Turn underline on and back off again
+        int const line_width = w; //Actually drawing the message
+        return cursor_mvmt + set_underline + line_width;
+    } else {
+        return 0;
+    }
+}
+
+//Tells item that it should redraw, probably because it moved to another
+//area of the screen
+void trigger_redraw_inter(void *item) {
+    return;
+}
+
+guv_operations const default_guv_ops = {
+    got_line_inter,
+    lines_req_inter,
+    {
+        draw_fn_inter,
+        draw_sz_inter,
+        trigger_redraw_inter
+    }
+};
---
+++ timonier.h
@@ -0,0 +1,22 @@
+#ifndef TIMONIER_H
+#define TIMONIER_H 1
+
+//This file collects some different dbg_guv managers
+
+#include "dbg_cmd.h"
+#include "twm.h"
+#include "dbg_guv.h"
+
+//Default manager: defaults to parsing a "set" command and has no associated
+//state struct
+extern guv_operations const default_guv_ops;
+
+//File transmission manager
+typedef struct _file_tx_timonier {
+    int fd;
+    //etc, I'll finish this once I have the basic stuff up and running
+} file_tx_manager;
+
+//extern guv_operations const file_tx_guv_ops;
+
+#endif
